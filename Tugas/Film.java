3f0616cc4b1ca0637b9b7dbb197483706f85197a
AhmadAriaAS <razscer0@gmail.com>
2023-06-13 12:51:49 +0700

    Tugas 1 Versi Advance

--- Tugas/Film.java
+++ Tugas/Film.java
@@ -1,5 +1,41 @@
 package Tugas;
 
+import java.util.Stack;
+
 public class Film {
-    
+    Stack<Node> stck = new Stack<>();
+
+    int id = 1;
+
+    public int incrementeId() {
+        return id++;
+    }
+
+    public void pusher(Node data) {
+        stck.push(data);
+    }
+
+    public Node popValue() throws Exception {
+        if (stck.empty())
+            throw new Exception("Stack masih kosong!");
+
+        return stck.pop();
+    }
+
+    public void peekValue() {
+        Node node = stck.peek();
+
+        System.out.printf("Film{ID Film: %s, Judul Film: %s, Tahun Tayang: %s, Director: %s}\n", node.getId(),
+                node.getTitle(), node.getReleaseYear(), node.getReleaseYear());
+    }
+
+    public void printAll() throws Exception {
+        if (stck.empty())
+            throw new Exception("Stack masih kosong!");
+
+        for (Node node : stck) {
+            System.out.printf("Film{ID Film: %s, Judul Film: %s, Tahun Tayang: %s, Director: %s}\n", node.getId(),
+                    node.getTitle(), node.getReleaseYear(), node.getReleaseYear());
+        }
+    }
 }
