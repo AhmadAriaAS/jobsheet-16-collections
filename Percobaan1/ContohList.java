a7d4b2e41b9b2f92e3000df9075a2c1888b502ec
MaulidinZakaria <maulidinzakaria123@gmail.com>
2023-06-13 11:43:51 +0700

    nambah list baru <string>

--- Percobaan1/ContohList.java
+++ Percobaan1/ContohList.java
@@ -1,11 +1,34 @@
 package Percobaan1;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ContohList {
    public static void main(String[] args) {
-        List l = new ArrayList();
-        l.add
+        List<integer> l = new ArrayList();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add("Cireng");
+        System.out.printf("Elemen 0 : %d total elemen : %d elemen terakhir : %sn", l.get(0), l.size(), l.get(l.size()-1));
+
+        l.add(4);
+        l.remove(0);
+        System.out.printf("Elemen 0 : %d total elemen : %d elemen terakhir : %sn", l.get(0), l.size(), l.get(l.size()-1));
+
+        List<String> names = new LinkedList<>();
+        names.add("Noureen");
+        names.add("Akhleema");
+        names.add("Shannum");
+        names.add("Uwais");
+        names.add("AL-Qarni");
+
+        System.out.printf("Elemen 0 : %s total elemen : %s elemen terakhir : %sn", names.get(0), names.size(), names.get(names.size()-1));
+        
+        names.set(0, "My kid");
+        System.out.printf("Elemen 0 : %s total elemen : %s elemen terakhir : %sn", names.get(0), names.size(), names.get(names.size()-1));
+        System.out.println("Names : " + names.toString());
+
    } 
 }
