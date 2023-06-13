9aa853fffefaf16ca003e07061c15eb69081a728
MaulidinZakaria <maulidinzakaria123@gmail.com>
2023-06-13 11:16:41 +0700

    first commit

---
+++ Percobaan1/ContohList.java
@@ -0,0 +1,11 @@
+package Percobaan1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContohList {
+   public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add
+   } 
+}
